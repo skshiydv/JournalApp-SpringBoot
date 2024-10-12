619646739671d082862d5e5eaef97e57c7812190
skshiydv <skshiydv@gmail.com>
2024-10-12 20:52:25 +0530

    external api integration

---
+++ src/main/java/io/github/skshiydv/journalapp/ExternalAPI/response.java
@@ -0,0 +1,12 @@
+package io.github.skshiydv.journalapp.ExternalAPI;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class response {
+    public String quote;
+    public String author;
+    public String category;
+}
