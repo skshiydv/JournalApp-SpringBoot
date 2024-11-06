76f05d9cbab9c9b3e97f1e90a31345b0210ee945
skshiydv <skshiydv@gmail.com>
2024-11-06 20:41:47 +0530

    email configuration

--- src/main/java/io/github/skshiydv/journalapp/JournalAppApplication.java
+++ src/main/java/io/github/skshiydv/journalapp/JournalAppApplication.java
@@ -5,11 +5,13 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.data.mongodb.MongoDatabaseFactory;
 import org.springframework.data.mongodb.MongoTransactionManager;
+import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
 import org.springframework.transaction.PlatformTransactionManager;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
+@EnableMongoRepositories
 @EnableTransactionManagement
 public class JournalAppApplication {
 
