9eba2cabed0469d76bc705c5a83faf4778b13895
skshiydv <skshiydv@gmail.com>
2025-02-05 09:33:26 +0530

    commit

--- src/main/java/io/github/skshiydv/journalapp/services/userService.java
+++ src/main/java/io/github/skshiydv/journalapp/services/userService.java
@@ -14,38 +14,43 @@ import java.util.List;
 @Service
 @RequiredArgsConstructor
 public class userService {
-    private final UserRepository userRepository;
     private static final PasswordEncoder passwordEncoder = new BCryptPasswordEncoder();
+    private final UserRepository userRepository;
+
     public void saveNewUser(User user) {
         user.setPassword(passwordEncoder.encode(user.getPassword()));
-        user.setRoles(Arrays.asList("USER"));
+        user.setRoles(List.of("USER"));
         userRepository.save(user);
 
 
     }
+
     public boolean save(User user) {
         try {
             userRepository.save(user);
             return true;
-        }
-        catch (Exception e) {
+        } catch (Exception e) {
             return false;
         }
 
 
     }
+
     public List<User> findAll() {
         return userRepository.findAll();
     }
+
     public void delete(ObjectId id) {
         userRepository.deleteById(id);
     }
+
     public User findById(ObjectId id) {
-        if(userRepository.findById(id).isPresent()) {
+        if (userRepository.findById(id).isPresent()) {
             return userRepository.findById(id).get();
         }
         return null;
     }
+
     public User findByUsername(String username) {
         return userRepository.findByUsername(username);
     }
@@ -56,7 +61,7 @@ public class userService {
 
     public void saveAdmin(User user) {
         user.setPassword(passwordEncoder.encode(user.getPassword()));
-        user.setRoles(Arrays.asList("ADMIN","USER"));
+        user.setRoles(Arrays.asList("ADMIN", "USER"));
         userRepository.save(user);
     }
 }
